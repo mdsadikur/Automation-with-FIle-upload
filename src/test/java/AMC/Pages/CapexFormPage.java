2ca4ecc614c346a0b4bc95f23e60eac523d7f015
unknown <sadikur.rahman3168@gmail.com>
2023-11-06 10:50:05 +0600

    file upload updated

--- src/test/java/AMC/Pages/CapexFormPage.java
+++ src/test/java/AMC/Pages/CapexFormPage.java
@@ -29,7 +29,7 @@ public class CapexFormPage extends BaseAMC {
         return this;
     }*/
 
-    public CapexFormPage getimage () throws InterruptedException, AWTException {
+    /*public CapexFormPage getimage () throws InterruptedException, AWTException {
         System.out.println("click on the " + fileUploadBtn);
         fileUploadBtn.click();
         String filePath2=commonMethod.getFilePath("Man.jpg");
@@ -38,7 +38,21 @@ public class CapexFormPage extends BaseAMC {
         return this;
 
     }
+*/
+    public CapexFormPage getimage() throws InterruptedException {
+//        WebElement fileUpload = driver.findElement(By.xpath("//input[@id='file_uploaded']"));
 
+        // Construct the relative path to your file
+        String relativeFilePath = "\\src\\test\\File\\Man.jpg";
+
+        String projectFolderPath = System.getProperty("user.dir");
+        String absoluteFilePath = projectFolderPath + relativeFilePath;
+
+        fileUploadBtn.sendKeys(absoluteFilePath);
+        Thread.sleep(5000); // Wait 5 seconds (you may consider using explicit waits instead)
+
+        return this;
+    }
 
 
 
